0b14faae4dce4ea85bfcfe2c98a0650c3d08c89f
Syl Turner <syl.turner@gmail.com>
2011-07-07 23:54:35 -0400

    Possibly fixing some problem with SSL on the production server with Facebook.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   Settings = YAML.load_file("#{RAILS_ROOT}/config/application.yml")[RAILS_ENV]
   provider :twitter, Settings["twitter"]["consumer_key"], Settings["twitter"]["consumer_secret"]
-  provider :facebook, Settings["facebook"]["app_id"], Settings["facebook"]["app_secret"], {:scope => "user_about_me"}
+  provider :facebook, Settings["facebook"]["app_id"], Settings["facebook"]["app_secret"], {:scope => "user_about_me", :client_options => {:ssl => {:ca_path => Settings["facebook"]["ca_path"]}}}
 end
 
 # uncomment to debug errors from omniauth
