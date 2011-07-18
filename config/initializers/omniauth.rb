6887718b99f9b42e4df9fad2788f57818610faef
Syl Turner <syl.turner@gmail.com>
2011-07-18 16:36:30 -0400

    Moving the ssl_ca_path outside of facebook/twitter because both apparently need to use it now.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
   Settings = YAML.load_file("#{RAILS_ROOT}/config/application.yml")[RAILS_ENV]
-  provider :twitter, Settings["twitter"]["consumer_key"], Settings["twitter"]["consumer_secret"]
-  provider :facebook, Settings["facebook"]["app_id"], Settings["facebook"]["app_secret"], {:client_options => {:ssl => {:ca_path => Settings["facebook"]["ssl_ca_path"]}}, :scope => "user_about_me"}
+  provider :twitter, Settings["twitter"]["consumer_key"], Settings["twitter"]["consumer_secret"], {:client_options => {:ssl => {:ca_path => Settings["ssl_ca_path"]}}}
+  provider :facebook, Settings["facebook"]["app_id"], Settings["facebook"]["app_secret"], {:client_options => {:ssl => {:ca_path => Settings["ssl_ca_path"]}}, :scope => "user_about_me"}
 end
 
 # uncomment to debug errors from omniauth
