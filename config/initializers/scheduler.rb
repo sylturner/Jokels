ec4d9915dadd4e533f9778883adbfc7a8db9d0e0
Syl Turner <syl.turner@gmail.com>
2011-08-22 00:21:53 -0400

    Added a tweet to say what the daily inspiration is. Updated the word list.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,6 +4,9 @@ require 'rss/2.0'
 
 scheduler = Rufus::Scheduler.start_new
 
+jokels_user = User.find 1
+@client = Twitter::Client.new(:oauth_token => jokels_user.token, :oauth_token_secret => jokels_user.secret)
+
 # every day at 10:00am
 scheduler.cron '00 10 * * 0-6 America/New_York' do
    	# set random word of the day
@@ -26,4 +29,19 @@ scheduler.cron '00 10 * * 0-6 America/New_York' do
         joke.save!
       end
     end
+    @client.update("Today's inspiration: " + DailyWord.last[:word])
 end
+
+# every day at 12:00pm
+#scheduler.cron '00 12 * * 0-6 America/New_York' do
+#  
+#  top_joke = Joke.where(['created_at BETWEEN ? AND ?', Date.yesterday, Time.now]).sort_by{|x| x.votes}.reverse[0]
+#  if top_joke
+#    tweet = "Yesterday's top joke: "
+#    user = top_joke.user
+#    if user
+#      
+#    end
+#    client.update(@joke.question + " " + url_for(@joke))
+#  end
+#end
