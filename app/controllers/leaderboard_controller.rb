96a26c63e305a92e4838a1211940a83abf02719d
Adam Griffis <adam.griffis@hannonhill.com>
2012-04-06 00:30:53 -0400

    [#15] Hopefully this will fix the problem on jokels.com :(

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -31,24 +31,24 @@ class LeaderboardController < ApplicationController
        if @sort == "most"
          case @time
           when "today"
-            user_jokes = Joke.select("user_id, count(1) as total_jokes").where(['created_at BETWEEN ? AND ?', Date.today, Date.tomorrow - 1.minute]).group("user_id").having("count(1) > 0").sort_by{|x| (x.total_jokes)}.reverse[0...10]
+            user_jokes = Joke.select("user_id, count(1) as total_jokes").where(['user_id is not null and created_at BETWEEN ? AND ?', Date.today, Date.tomorrow - 1.minute]).group("user_id").having("count(1) > 0").sort_by{|x| (x.total_jokes)}.reverse[0...10]
           when "week"
-            user_jokes = Joke.select("user_id, count(1) as total_jokes").where(['created_at BETWEEN ? AND ?', Time.now.beginning_of_week, Time.now]).group("user_id").having("count(1) > 0").sort_by{|x| (x.total_jokes)}.reverse[0...10]
+            user_jokes = Joke.select("user_id, count(1) as total_jokes").where(['user_id is not null and created_at BETWEEN ? AND ?', Time.now.beginning_of_week, Time.now]).group("user_id").having("count(1) > 0").sort_by{|x| (x.total_jokes)}.reverse[0...10]
           when "month"
-            user_jokes = Joke.select("user_id, count(1) as total_jokes").where(['created_at BETWEEN ? AND ?', Time.now.beginning_of_month, Time.now]).group("user_id").having("count(1) > 0").sort_by{|x| (x.total_jokes)}.reverse[0...10]
+            user_jokes = Joke.select("user_id, count(1) as total_jokes").where(['user_id is not null and created_at BETWEEN ? AND ?', Time.now.beginning_of_month, Time.now]).group("user_id").having("count(1) > 0").sort_by{|x| (x.total_jokes)}.reverse[0...10]
           when "all-time"
-            user_jokes = Joke.select("user_id, count(1) as total_jokes").sort_by{|x| (x.total_jokes)}.reverse[0...10]
+            user_jokes = Joke.select("user_id, count(1) as total_jokes").where("user_id is not null").sort_by{|x| (x.total_jokes)}.reverse[0...10]
           end
        else
          case @time
          when "today"
-           user_jokes = Joke.select("user_id, sum((up_votes - down_votes)) as total_votes").where(['created_at BETWEEN ? AND ?', Date.today, Date.tomorrow - 1.minute]).group("user_id").having("sum(up_votes + down_votes) > 0").sort_by{|x| (sort_direction*(x.total_votes||=0))}.reverse[0...10]
+           user_jokes = Joke.select("user_id, sum((up_votes - down_votes)) as total_votes").where(['user_id is not null and created_at BETWEEN ? AND ?', Date.today, Date.tomorrow - 1.minute]).group("user_id").having("sum(up_votes + down_votes) > 0").sort_by{|x| (sort_direction*(x.total_votes||=0))}.reverse[0...10]
          when "week"
-           user_jokes = Joke.select("user_id, sum((up_votes - down_votes)) as total_votes").where(['created_at BETWEEN ? AND ?', Time.now.beginning_of_week, Time.now]).group("user_id").having("sum(up_votes + down_votes) > 0").sort_by{|x| (sort_direction*(x.total_votes||=0))}.reverse[0...10]
+           user_jokes = Joke.select("user_id, sum((up_votes - down_votes)) as total_votes").where(['user_id is not null and created_at BETWEEN ? AND ?', Time.now.beginning_of_week, Time.now]).group("user_id").having("sum(up_votes + down_votes) > 0").sort_by{|x| (sort_direction*(x.total_votes||=0))}.reverse[0...10]
          when "month"
-           user_jokes = Joke.select("user_id, sum((up_votes - down_votes)) as total_votes").where(['created_at BETWEEN ? AND ?', Time.now.beginning_of_month, Time.now]).group("user_id").having("sum(up_votes + down_votes) > 0").sort_by{|x| (sort_direction*(x.total_votes||=0))}.reverse[0...10]
+           user_jokes = Joke.select("user_id, sum((up_votes - down_votes)) as total_votes").where(['user_id is not null and created_at BETWEEN ? AND ?', Time.now.beginning_of_month, Time.now]).group("user_id").having("sum(up_votes + down_votes) > 0").sort_by{|x| (sort_direction*(x.total_votes||=0))}.reverse[0...10]
          when "all-time"
-           user_jokes = Joke.select("user_id, sum((up_votes - down_votes)) as total_votes").sort_by{|x| (sort_direction*x.total_votes)}.reverse[0...10]
+           user_jokes = Joke.select("user_id, sum((up_votes - down_votes)) as total_votes").where("user_id is not null").group("user_id").sort_by{|x| (sort_direction*x.total_votes)}.reverse[0...10]
          when "newest"
            @users = User.all.reverse[0...10]
          end
