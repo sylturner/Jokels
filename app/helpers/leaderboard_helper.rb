9dc7311f016fe02174861b84ccb381dda7460359
Syl Turner <syl.turner@gmail.com>
2012-04-07 18:45:43 -0400

    [#13] - Using the 'mini' buttons for the leaderboard buttons because they are overflowing. Added the 'Most' sort option when looking at the Users leaderboard. Removed the footer from the mobile version since it was messing up badly when scrolling (see https://github.com/scottjehl/Device-Bugs/issues/1). Since the footer is gone, I moved the buttons to the header. Put the 'full version' as a small link at the bottom of the page instead of as a button in the header.

--- app/helpers/leaderboard_helper.rb
+++ app/helpers/leaderboard_helper.rb
@@ -6,6 +6,6 @@ module LeaderboardHelper
 
 	def leaderboard_sorting_button time, sort, sort_type, link_text, condition
 		css_class = "ui-btn-active" if condition
-		link_to link_text, generate_leaderboard_link(time,sort,sort_type), "data-role" => "button", "data-ajax" => "false", :class => css_class
+		link_to link_text, generate_leaderboard_link(time,sort,sort_type), "data-role" => "button", "data-ajax" => "false", "data-mini" => "true", :class => css_class
 	end
 end
