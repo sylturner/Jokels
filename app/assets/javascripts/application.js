d0026ec27ad264402c8e3a7f62ff2f60be786f17
Syl Turner <syl.turner@gmail.com>
2013-12-08 12:34:58 -0500

    wip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,18 @@
 ////= require_self
 ////= require_directory .
 //= require_tree ../../../vendor/assets/javascripts/.
+$('#add-joke').on('shown.bs.modal', function() {
+  $("#joke_question").focus();
+});
+
+$("body").delegate(".reveal-punchline", "click", function() {
+  $("#click-for-punchline").hide();
+  $("#answer-container").slideDown('fast');
+  $("#question").css('cursor', 'default');
+  $("#share").delay(750).slideDown('fast');
+  $("#refresh-joke").delay(750).slideDown('fast');
+  $("#alternate-punchlines-button").delay(750).slideDown('fast');
+});
+
+$("body").delegate(".refresh-joke", "click", function() {
+});
